a787cde4f75ec1e66148053564962a8751add431
keeyousha <keeyousha@gmail.com>
2022-10-21 05:01:25 +0300

    Add project files.

---
+++ Task1/Task1.cs
@@ -0,0 +1,73 @@
+﻿namespace Task1
+{
+    public class Task1
+    {
+/*
+ * Задание 1.1. Дана строка. Верните строку, содержащую текст "Длина: NN",
+ * где NN — длина заданной строки. Например, если задана строка "hello",
+ * то результатом должна быть строка "Длина: 5".
+ */
+        internal static int StringLength(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+/*
+ * Задание 1.2. Дана непустая строка. Вернуть коды ее первого и последнего символов.
+ * Рекомендуется найти специальные функции для вычисления соответствующих символов и их кодов.
+ */
+        internal static Tuple<int?, int?> FirstLastCodes(string s)
+        {
+            return new Tuple<int?, int?>(Code(First(s)), Code(Last(s)));
+        }
+        
+        private static char? First(string s) => throw new NotImplementedException(); 
+        private static char? Last(string s) => throw new NotImplementedException();
+        private static int? Code(char? c) => throw new NotImplementedException();
+       
+
+/*
+ * Задание 1.3. Дана строка. Подсчитать количество содержащихся в ней цифр.
+ * В решении необходимо воспользоваться циклом for.
+ */
+        internal static int CountDigits(string s)
+        {
+            throw new NotImplementedException();
+        }
+
+/*
+ * Задание 1.4. Дана строка. Подсчитать количество содержащихся в ней цифр.
+ * В решении необходимо воспользоваться методом Count:
+ * https://docs.microsoft.com/ru-ru/dotnet/api/system.linq.enumerable.count?view=net-6.0#system-linq-enumerable-count-1(system-collections-generic-ienumerable((-0))-system-func((-0-system-boolean)))
+ * и функцией Char.IsDigit:
+ * https://docs.microsoft.com/ru-ru/dotnet/api/system.char.isdigit?view=net-6.0
+ */
+        internal static int CountDigits2(string s)
+        {
+            throw new NotImplementedException();
+        }
+        
+/*
+ * Задание 1.5. Дана строка, изображающая арифметическое выражение вида «<цифра>±<цифра>±…±<цифра>»,
+ * где на месте знака операции «±» находится символ «+» или «−» (например, «4+7−2−8»). Вывести значение
+ * данного выражения (целое число).
+ */
+        internal static int CalcDigits(string expr) {
+            throw new NotImplementedException();
+        }
+
+/*
+ * Задание 1.6. Даны строки S, S1 и S2. Заменить в строке S первое вхождение строки S1 на строку S2.
+ */
+        internal static string ReplaceWithString(string s, string s1, string s2) {
+            throw new NotImplementedException();
+        }
+        
+
+        public static void Main(string[] args)
+        {
+            throw new NotImplementedException(
+                "Вызовите здесь все перечисленные в классе функции, как это сделано в предыдущих заданиях");
+        }
+    }
+}
